43cee2afed8c698cfe26c48d0eac7a07258ea156
gamerOZZY <ozzy.ra01@gmail.com>
2025-11-30 22:00:58 -0600

    pruebanumero5

--- MetodosNumericos/der123.Designer.cs
+++ MetodosNumericos/der123.Designer.cs
@@ -29,49 +29,25 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.asd = new System.Windows.Forms.ComboBox();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(374, 203);
+            this.label1.Location = new System.Drawing.Point(384, 157);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 16);
+            this.label1.Size = new System.Drawing.Size(128, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "prueba3";
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(210, 228);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
-            this.dataGridView1.TabIndex = 1;
-            // 
-            // asd
-            // 
-            this.asd.FormattingEnabled = true;
-            this.asd.Location = new System.Drawing.Point(170, 157);
-            this.asd.Name = "asd";
-            this.asd.Size = new System.Drawing.Size(121, 24);
-            this.asd.TabIndex = 2;
+            this.label1.Text = "pruebanumero5";
             // 
             // der123
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.asd);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(640, 360);
             this.Controls.Add(this.label1);
             this.Name = "der123";
             this.Text = "der123";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -80,7 +56,5 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.ComboBox asd;
     }
 }
