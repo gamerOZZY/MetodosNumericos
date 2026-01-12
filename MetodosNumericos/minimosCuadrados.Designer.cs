03999ad19da9314820aac02636154ed6d5b0f8b8
edanrp <ureynosoedgar@gmail.com>
2026-01-11 20:49:10 -0600

    waaaaaaaaaaaar isssssssss ooooooooooveeerrr lo dimos todo en este proyecto ggggggggggggg

--- MetodosNumericos/minimosCuadrados.Designer.cs
+++ MetodosNumericos/minimosCuadrados.Designer.cs
@@ -51,9 +51,10 @@
             // 
             // pbGrafica
             // 
-            this.pbGrafica.Location = new System.Drawing.Point(915, 430);
+            this.pbGrafica.Location = new System.Drawing.Point(654, 339);
+            this.pbGrafica.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pbGrafica.Name = "pbGrafica";
-            this.pbGrafica.Size = new System.Drawing.Size(814, 563);
+            this.pbGrafica.Size = new System.Drawing.Size(510, 389);
             this.pbGrafica.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbGrafica.TabIndex = 0;
             this.pbGrafica.TabStop = false;
@@ -61,50 +62,56 @@
             // dgvDatos
             // 
             this.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvDatos.Location = new System.Drawing.Point(261, 12);
+            this.dgvDatos.Location = new System.Drawing.Point(580, 19);
+            this.dgvDatos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dgvDatos.Name = "dgvDatos";
             this.dgvDatos.RowHeadersWidth = 51;
             this.dgvDatos.RowTemplate.Height = 24;
-            this.dgvDatos.Size = new System.Drawing.Size(864, 364);
+            this.dgvDatos.Size = new System.Drawing.Size(648, 296);
             this.dgvDatos.TabIndex = 1;
             // 
             // txtX
             // 
-            this.txtX.Location = new System.Drawing.Point(26, 23);
+            this.txtX.Location = new System.Drawing.Point(136, 61);
+            this.txtX.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtX.Name = "txtX";
-            this.txtX.Size = new System.Drawing.Size(100, 22);
+            this.txtX.Size = new System.Drawing.Size(76, 27);
             this.txtX.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 4);
+            this.label1.Location = new System.Drawing.Point(134, 39);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(16, 16);
+            this.label1.Size = new System.Drawing.Size(19, 20);
             this.label1.TabIndex = 3;
             this.label1.Text = "xi";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(129, 4);
+            this.label2.Location = new System.Drawing.Point(214, 39);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(17, 16);
+            this.label2.Size = new System.Drawing.Size(20, 20);
             this.label2.TabIndex = 5;
             this.label2.Text = "yi";
             // 
             // txtY
             // 
-            this.txtY.Location = new System.Drawing.Point(132, 23);
+            this.txtY.Location = new System.Drawing.Point(218, 61);
+            this.txtY.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtY.Name = "txtY";
-            this.txtY.Size = new System.Drawing.Size(100, 22);
+            this.txtY.Size = new System.Drawing.Size(76, 27);
             this.txtY.TabIndex = 4;
             // 
             // btnAgregar
             // 
-            this.btnAgregar.Location = new System.Drawing.Point(90, 51);
+            this.btnAgregar.Location = new System.Drawing.Point(366, 55);
+            this.btnAgregar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.Size = new System.Drawing.Size(75, 33);
             this.btnAgregar.TabIndex = 6;
             this.btnAgregar.Text = "agregar";
             this.btnAgregar.UseVisualStyleBackColor = true;
@@ -112,42 +119,47 @@
             // 
             // cboTipo
             // 
-            this.cboTipo.Location = new System.Drawing.Point(77, 111);
+            this.cboTipo.Location = new System.Drawing.Point(276, 131);
+            this.cboTipo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cboTipo.Name = "cboTipo";
-            this.cboTipo.Size = new System.Drawing.Size(121, 24);
+            this.cboTipo.Size = new System.Drawing.Size(92, 28);
             this.cboTipo.TabIndex = 0;
             this.cboTipo.SelectedIndexChanged += new System.EventHandler(this.cboTipo_SelectedIndexChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(74, 92);
+            this.label3.Location = new System.Drawing.Point(147, 134);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(103, 16);
+            this.label3.Size = new System.Drawing.Size(107, 20);
             this.label3.TabIndex = 7;
             this.label3.Text = "Tipo de metodo";
             // 
             // numGrado
             // 
-            this.numGrado.Location = new System.Drawing.Point(77, 160);
+            this.numGrado.Location = new System.Drawing.Point(278, 193);
+            this.numGrado.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.numGrado.Name = "numGrado";
-            this.numGrado.Size = new System.Drawing.Size(120, 22);
+            this.numGrado.Size = new System.Drawing.Size(90, 27);
             this.numGrado.TabIndex = 8;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(74, 141);
+            this.label4.Location = new System.Drawing.Point(206, 195);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(45, 16);
+            this.label4.Size = new System.Drawing.Size(48, 20);
             this.label4.TabIndex = 9;
             this.label4.Text = "Grado";
             // 
             // btnCalcular
             // 
-            this.btnCalcular.Location = new System.Drawing.Point(26, 202);
+            this.btnCalcular.Location = new System.Drawing.Point(210, 235);
+            this.btnCalcular.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCalcular.Name = "btnCalcular";
-            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.Size = new System.Drawing.Size(93, 45);
             this.btnCalcular.TabIndex = 10;
             this.btnCalcular.Text = "Calcular";
             this.btnCalcular.UseVisualStyleBackColor = true;
@@ -155,9 +167,10 @@
             // 
             // btnLimpiar
             // 
-            this.btnLimpiar.Location = new System.Drawing.Point(157, 202);
+            this.btnLimpiar.Location = new System.Drawing.Point(348, 237);
+            this.btnLimpiar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnLimpiar.Name = "btnLimpiar";
-            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.Size = new System.Drawing.Size(93, 43);
             this.btnLimpiar.TabIndex = 11;
             this.btnLimpiar.Text = "Reiniciar";
             this.btnLimpiar.UseVisualStyleBackColor = true;
@@ -166,28 +179,31 @@
             // lblEcuacion
             // 
             this.lblEcuacion.AutoSize = true;
-            this.lblEcuacion.Font = new System.Drawing.Font("Unispace", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEcuacion.Location = new System.Drawing.Point(134, 388);
+            this.lblEcuacion.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEcuacion.Location = new System.Drawing.Point(150, 317);
+            this.lblEcuacion.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblEcuacion.Name = "lblEcuacion";
-            this.lblEcuacion.Size = new System.Drawing.Size(130, 24);
+            this.lblEcuacion.Size = new System.Drawing.Size(99, 20);
             this.lblEcuacion.TabIndex = 12;
             this.lblEcuacion.Text = "__________";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Unispace", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(22, 388);
+            this.label6.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(66, 317);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(106, 24);
+            this.label6.Size = new System.Drawing.Size(68, 20);
             this.label6.TabIndex = 13;
             this.label6.Text = "Ecuacion";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(90, 563);
+            this.button1.Location = new System.Drawing.Point(46, 700);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(74, 28);
             this.button1.TabIndex = 14;
             this.button1.Text = "Salir";
             this.button1.UseVisualStyleBackColor = true;
@@ -195,9 +211,10 @@
             // 
             // minimosCuadrados
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1741, 994);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoScroll = true;
+            this.BackgroundImage = global::MetodosNumericos.Properties.Resources.cuaderno;
+            this.ClientSize = new System.Drawing.Size(1370, 749);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.lblEcuacion);
@@ -214,8 +231,12 @@
             this.Controls.Add(this.txtX);
             this.Controls.Add(this.dgvDatos);
             this.Controls.Add(this.pbGrafica);
+            this.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "minimosCuadrados";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "minimosCuadrados";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             ((System.ComponentModel.ISupportInitialize)(this.pbGrafica)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numGrado)).EndInit();
